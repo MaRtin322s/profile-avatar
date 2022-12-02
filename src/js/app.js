280e19e00c7ebc0b7ec7a3591a963800c7ff2cc6
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-02 16:58:16 +0200

    scale image twice

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+    const image = document.querySelector('.image img');
+    image.addEventListener('click', () => {
+        image.style.transform = "scale(2)";
+    });
+
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+        alert("💣");
+    });
 });
